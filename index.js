1dde67f4420d9ede0ca92e66b16afda96d359b68
Greyson Harris <gharris@esri.com>
2018-12-18 01:11:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+let driver = {};
+
+//Not destructive
+function updateDriverWithKeyAndValue(key1, key2) {
+  return driver
+}
+
+//Destructive
+function destructivelyUpdateDriverWithKeyAndValue() {
+  
+}
+
+//Not destructive
+function deleteFromDriverByKey() {
+  
+}
+
+//Destructive
+function destructivelyDeleteFromDriverByKey() {
+  
+}
